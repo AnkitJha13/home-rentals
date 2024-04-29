9844ea36cfc156a6b415f4221b79853363d0695d
Ankit <ankitjha1403@gmail.com>
2024-04-29 22:51:02 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+const path = require("path");
 
 const authRoutes = require("./routes/auth.js")
 const listingRoutes = require("./routes/listing.js")
 const bookingRoutes = require("./routes/booking.js")
 const userRoutes = require("./routes/user.js")
+
+
 
 app.use(cors());
 app.use(express.json());
@@ -18,6 +21,8 @@ app.use("/auth", authRoutes)
 app.use("/properties", listingRoutes)
 app.use("/bookings", bookingRoutes)
 app.use("/users", userRoutes)
+
+
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 3001;
