2f6d4e7f87c0e14a93941e654f7720fb014b0b8d
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-05-01 17:49:42 +0530

    Update Listings.jsx

--- client/src/components/Listings.jsx
+++ client/src/components/Listings.jsx
@@ -18,8 +18,8 @@ const Listings = () => {
     try {
       const response = await fetch(
         selectedCategory !== "All"
-          ? `http://localhost:3001/properties?category=${selectedCategory}`
-          : "http://localhost:3001/properties",
+          ? `https://home-rentals-6fme.onrender.com/properties?category=${selectedCategory}`
+          : "https://home-rentals-6fme.onrender.com/properties",
         {
           method: "GET",
         }
