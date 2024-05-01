f2b0bb92c7d5ae22779f546f6c3132f221cb812f
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-05-01 17:50:06 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -60,7 +60,7 @@ const Navbar = () => {
             <Person sx={{ color: variables.darkgrey }} />
           ) : (
             <img
-              src={`http://localhost:3001/${user.profileImagePath.replace(
+              src={`https://home-rentals-6fme.onrender.com/${user.profileImagePath.replace(
                 "public",
                 ""
               )}`}
