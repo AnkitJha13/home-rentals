da4f08f390cbf4fe9efc206e08531c325bb616e9
Ankit <ankitjha1403@gmail.com>
2024-05-01 11:11:13 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -3,14 +3,11 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
-const path = require("path");
 
 const authRoutes = require("./routes/auth.js")
 const listingRoutes = require("./routes/listing.js")
 const bookingRoutes = require("./routes/booking.js")
 const userRoutes = require("./routes/user.js")
-
-
 
 app.use(cors());
 app.use(express.json());
@@ -22,10 +19,8 @@ app.use("/properties", listingRoutes)
 app.use("/bookings", bookingRoutes)
 app.use("/users", userRoutes)
 
-
-
 /* MONGOOSE SETUP */
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 mongoose
   .connect(process.env.MONGO_URL, {
     dbName: "Dream_Nest",
