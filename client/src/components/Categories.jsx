483df9c02ead65999ccd545bde24e905294dd364
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-05-03 11:17:46 +0530

    Update Categories.jsx

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -14,8 +14,8 @@ const Categories = () => {
 
       <div className="categories_list">
         {categories?.slice(1, 7).map((category, index) => (
-          <Link to={`/properties/category/${category.label}`} key={index}>
-            <div className="category" >
+          <Link to={`/properties/category/${category.label}`}>
+            <div className="category" key={index}>
               <img src={category.img} alt={category.label} />
               <div className="overlay"></div>
               <div className="category_text">
