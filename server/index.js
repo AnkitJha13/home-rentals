d89a2137d40f7a385b92d3e34f2efd7124bb7b79
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-05-03 11:00:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,28 +2,29 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
-const cors = require("cors");
+const cors = require("cors")
 
 const authRoutes = require("./routes/auth.js")
 const listingRoutes = require("./routes/listing.js")
-const bookingRoutes = require("./routes/booking.js")
 const userRoutes = require("./routes/user.js")
+const bookingRoutes = require("./routes/booking.js")
 
-app.use(cors());
+app.use(cors()); // Block other request which is not from your domain
 app.use(express.json());
-app.use(express.static("public"));
+
+/* Serve static files from the "public" directory */
+app.use(express.static('public'));
 
 /* ROUTES */
 app.use("/auth", authRoutes)
 app.use("/properties", listingRoutes)
-app.use("/bookings", bookingRoutes)
 app.use("/users", userRoutes)
+app.use("/bookings", bookingRoutes)
 
 /* MONGOOSE SETUP */
-const PORT = 3001;
+const PORT = process.env.PORT || 5003;
 mongoose
   .connect(process.env.MONGO_URL, {
-    dbName: "Dream_Nest",
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
