d9f8d9052072d3c41acc0bd7b349d33b00da1ed0
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-05-03 11:45:12 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,7 +5,7 @@ const multer = require("multer");
 
 const User = require("../models/User");
 
-/* Configure Multer for FILE UPLOADS */
+/* Configuration Multer for File Upload */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/uploads/"); // Store uploaded files in the 'uploads' folder
@@ -18,20 +18,19 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /* USER REGISTER */
-
-router.post("/register", upload.single('profileImage'), async (req, res) => {
+router.post("/register", upload.single("profileImage"), async (req, res) => {
   try {
-    /* Take information from the form */
+    /* Take all information from the form */
     const { firstName, lastName, email, password } = req.body;
 
     /* The uploaded file is available as req.file */
     const profileImage = req.file;
 
     if (!profileImage) {
-      return res.status(400).send("No file uploaded.");
+      return res.status(400).send("No file uploaded");
     }
-    
-    /* Path to the uploaded profile photo */
+
+    /* path to the uploaded profile photo */
     const profileImagePath = profileImage.path;
 
     /* Check if user exists */
@@ -50,67 +49,52 @@ router.post("/register", upload.single('profileImage'), async (req, res) => {
       lastName,
       email,
       password: hashedPassword,
-      profileImagePath
+      profileImagePath,
     });
 
-    /* save new User */
-    await newUser.save()
+    /* Save the new User */
+    await newUser.save();
 
-    /* Send a success response */
+    /* Send a successful message */
     res
       .status(200)
       .json({ message: "User registered successfully!", user: newUser });
-    /* Handle any errors that occur during registration */
   } catch (err) {
     console.log(err);
     res
       .status(500)
-      .json({ message: "Registration failed", error: err.message });
+      .json({ message: "Registration failed!", error: err.message });
   }
 });
 
-/* USER LOGIN */
+/* USER LOGIN*/
 router.post("/login", async (req, res) => {
   try {
-    /* Take information from the form */
-    const { email, password } = req.body;
+    /* Take the infomation from the form */
+    const { email, password } = req.body
 
     /* Check if user exists */
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ message: "User doesn't exist!" });
+      return res.status(409).json({ message: "User doesn't exist!" });
     }
 
-    /* Compare password */
-    const isMatch = await bcrypt.compare(password, user.password);
+    /* Compare the password with the hashed password */
+    const isMatch = await bcrypt.compare(password, user.password)
     if (!isMatch) {
-      return res.status(400).json({ message: "Invalid credentials!" });
+      return res.status(400).json({ message: "Invalid Credentials!"})
     }
 
     /* Generate JWT token */
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    delete user.password;
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+    delete user.password
 
-    res.status(200).json({ token, user });
+    res.status(200).json({ token, user })
+
   } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: err.message });
+    console.log(err)
+    res.status(500).json({ error: err.message })
   }
-});
+})
 
-// /* GOOGLE LOGIN */
-// router.post("/googlelogin", async (req, res) => {
-//   try {
-//     /* Send the user data in the request body */
-//     const userData = req.body;
-
-//     res.status(200).json({ user: userData });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json({ error: err.message });
-//   }
-// });
-
-
-
-module.exports = router;
+module.exports = router
