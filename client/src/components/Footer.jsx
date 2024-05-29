30a6909a8e1c9c6c9cd640dfa0d1defd864c8943
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-05-29 21:35:53 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,30 +1,47 @@
-import "../styles/Footer.scss"
-import { LocationOn, LocalPhone, Email } from "@mui/icons-material"
+import "../styles/Footer.scss";
+import { LocationOn, LocalPhone, Email } from "@mui/icons-material";
 const Footer = () => {
   return (
     <div className="footer">
       <div className="footer_left">
-        <a href="/"><img src="/assets/logo.png" alt="logo" /></a>
+        <h3>CONNECT</h3>
+        <p>
+          Follow us on social media for the latest updates and exclusive offers.
+          Have questions or feedback? Reach out to our support team anytime.
+          We're here to make your Home booking experience seamless and
+          enjoyable!
+        </p>
       </div>
 
       <div className="footer_center">
-        <h3>Useful Links</h3>
+        <h3>COMPANY</h3>
         <ul>
-          <li><a href="#">About Us</a></li>
-          <li><a href="#">Terms and Conditions</a></li>
-          <li><a href="#">Return and Refund Policy</a></li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Terms and Conditions</a>
+          </li>
+          <li>
+            <a href="#">Return and Refund Policy</a>
+          </li>
         </ul>
       </div>
 
       <div className="footer_right">
-        <h3>Contact</h3>
+        <h3>CONTACT US</h3>
         <div className="footer_right_info">
           <LocalPhone />
           <p>+91 9876543210</p>
         </div>
         <div className="footer_right_info">
           <Email />
-          <p><a href="mailto:homehub@support.com">homehub@support.com</a></p>
+          
+            <p>homehub@support.com</p>
+         
         </div>
         <div className="footer_right_info">
           <LocationOn />
@@ -33,7 +50,7 @@ const Footer = () => {
         <img src="/assets/payment.png" alt="payment" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
